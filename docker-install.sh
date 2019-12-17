c9db61d6c0b01c41e31731ccee1abbcff5812d3d
chenbin <353549444@qq.com>
2019-12-17 15:19:07 +0800

    Update and rename test.sh to docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# install docker
+
+now=`date +%s`
+function log() {
+  echo "[$((`date +%s` - now ))] ## $@ ##"
+}
+
+log "Installation start at `date`"
+
+# check
+[[ `whoami` != "root" ]] && echo "Root Privilege needed, use sudo please." && exit 1
+OS=`awk -F= '/^NAME/{print $2}' /etc/os-release | sed "s/\"//g"`
+if [[ "$OS" == "Ubuntu" ]];then
+  :
+elif [[ "$OS" == "CentOS Linux" ]];then
+  OS="CentOS"
+else
+  echo "Unknown OS: \"$OS\", exit"
+  exit 2
+fi
+
+# install docker
+read -p "Install Docker?: " -ei $INSTALL_DOCKER INSTALL_DOCKER
+if [[ "$INSTALL_DOCKER" == "Y" ]];then
+  log "install and upgrade docker"
+  if [[ "$OS" == "Ubuntu" ]];then
+    apt-get update
+    apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository -u "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get -y install docker-ce
+  fi
+ 
+  if [[ "$OS" == "CentOS" ]];then
+    yum install -y yum-utils device-mapper-persistent-data lvm2
+    yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+    yum makecache fast
+    yum -y install docker-ce
+    systemctl enable docker && systemctl start docker  
+  fi
+fi
+
+# congifure mirror and insecure registries
+log "congifure mirror and insecure registries"
+cat > /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": ["https://m.mirror.aliyuncs.com"]
+}
+EOF
+systemctl daemon-reload && systemctl restart docker
+log "Installation end at `date`"
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 测试
-now=`date +%s`
-function log() {
-  echo "[$((`date +%s` - now ))] ## $@ ##"
-}
-
-log "Installation start at `date`"
